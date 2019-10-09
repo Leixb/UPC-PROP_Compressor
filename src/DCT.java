3948c3889b05929865f7e2d2d0a15be30a56eebf
Leixb <abone9999@gmail.com>
2019-10-09 21:35:08 +0200

    Implementat Quantization
    Cal testing
    
    closes #6

--- src/DCT.java
+++ src/DCT.java
@@ -146,4 +146,26 @@ public class DCT {
         }
         return RLE_table;
     }
+
+    final static private byte[][] QTable = {
+        { 16 , 11 , 10 , 16 ,  24 ,  40 ,  51 ,  61 },
+        { 12 , 12 , 14 , 19 ,  26 ,  58 ,  60 ,  55 },
+        { 14 , 13 , 16 , 24 ,  40 ,  57 ,  69 ,  56 },
+        { 14 , 17 , 22 , 29 ,  51 ,  87 ,  80 ,  62 },
+        { 18 , 22 , 37 , 56 ,  68 , 109 , 103 ,  77 },
+        { 24 , 35 , 55 , 64 ,  81 , 104 , 113 ,  92 },
+        { 49 , 64 , 78 , 87 , 103 , 121 , 120 , 101 },
+        { 72 , 92 , 95 , 98 , 112 , 100 , 103 ,  99 }
+    };
+
+    double QuantizationValue(short quality, short x, short y) throws IllegalArgumentException {
+        if (quality < 1 || quality > 100) throw new IllegalArgumentException("Quality must be between 1 and 100");
+        if (x < 0 || x >= 8) throw new IllegalArgumentException("0 < x < 8");
+        if (y < 0 || y >= 8) throw new IllegalArgumentException("0 < y < 8");
+
+        double q = 5000.0/quality;
+        if (quality >= 50) q = 200 - 2*quality;
+
+        return (q*QTable[x][y] + 50)/100;
+    }
 }

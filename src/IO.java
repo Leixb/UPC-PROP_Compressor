6eb25a5f71bb39e2b82b904760ccdf968e03e7b9
Albert Mercade <albertmercaplas@gmail.com>
2019-10-10 11:26:23 +0200

    IO acabat
    close #14

--- src/IO.java
+++ src/IO.java
@@ -2,27 +2,42 @@ import java.io.*;
 
 public class IO {
     public class reader {
-        public reader(String filename) {
+        private FileInputStream fin;
+        private BufferedInputStream bin;
 
+        public reader(String filename) throws FileNotFoundException {
+            fin = new FileInputStream(filename);
+            bin = new BufferedInputStream(fin);
         }
-        public byte[] read(byte[] buffer) {
+        public byte[] read(int n) throws IOException {
+            byte[] buffer = new byte[n];
+
+            bin.read(buffer);
 
             return buffer;
         }
-        public void close () {
-
+        public void close () throws IOException {
+            bin.close();
+            fin.close();
         }
     }
 
     public class writer {
-        public writer(String filename) {
+        private FileOutputStream fout;
+        private BufferedOutputStream bout;
 
+        public writer(String filename) throws FileNotFoundException {
+            fout = new FileOutputStream(filename);
+            bout = new BufferedOutputStream(fout);
         }
-        public void write(byte[] buffer) {
-
+        public void write(byte[] buffer) throws IOException {
+            bout.write(buffer);
         }
-        public void close () {
-
+        public void close () throws IOException {
+            bout.flush();
+            bout.close();
+            fout.flush();
+            fout.close();
         }
     }
 }

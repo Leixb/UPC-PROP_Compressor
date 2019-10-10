073cd9c7a4471b117b164a7070caa91d805a0270
Albert Mercade <albertmercaplas@gmail.com>
2019-10-10 10:56:34 +0200

    Creat classe IO
    (#14)

---
+++ src/IO.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class IO {
+    public class reader {
+        public reader(String filename) {
+
+        }
+        public byte[] read(byte[] buffer) {
+
+            return buffer;
+        }
+        public void close () {
+
+        }
+    }
+
+    public class writer {
+        public writer(String filename) {
+
+        }
+        public void write(byte[] buffer) {
+
+        }
+        public void close () {
+
+        }
+    }
+}

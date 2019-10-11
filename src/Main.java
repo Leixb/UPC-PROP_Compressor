eead06265786d326bee09ee6a6f364b8e421ec29
Albert Mercade <albertmercaplas@gmail.com>
2019-10-11 11:05:43 +0200

    Esquelet del main fet (PR #31)
    (#12)

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,115 @@
+import javax.sound.midi.Soundbank;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
+    private static String banner =
+            "██████╗ ██╗███████╗     ██████╗ ██████╗ ███╗   ███╗██████╗ ██████╗ ███████╗███████╗███████╗ ██████╗ ██████╗ \n" +
+            "██╔══██╗██║╚══███╔╝    ██╔════╝██╔═══██╗████╗ ████║██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔═══██╗██╔══██╗\n" +
+            "██████╔╝██║  ███╔╝     ██║     ██║   ██║██╔████╔██║██████╔╝██████╔╝█████╗  ███████╗███████╗██║   ██║██████╔╝\n" +
+            "██╔═══╝ ██║ ███╔╝      ██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██╔══██╗██╔══╝  ╚════██║╚════██║██║   ██║██╔══██╗\n" +
+            "██║     ██║███████╗    ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ██║  ██║███████╗███████║███████║╚██████╔╝██║  ██║\n" +
+            "╚═╝     ╚═╝╚══════╝     ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝\n" +
+            "                                                                                                            \n";
+
+    private static Scanner scanner;
+
     public static void main(String[] args) {
-        //System.out.println("Enter filename of ppm image:");
-        //Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
 
-        String filename = "images/gradient8.ppm";
-        String filename_out = "generated/test.ppm";
-        String filename_YCbCr = "generated/test_YCbCr.ppm";
-        String filename_RGB = "generated/test_RGB.ppm";
+        System.out.println("BENVINGUT A");
+        System.out.println(banner);
+        System.out.println("[1] Test");
+        System.out.println("[2] Normal");
+        short opt = scanner.nextShort();
 
-        Image image = null;
+        if(opt == 1) testMain();
+        else if(opt == 2) normalMain();
+        else quit();
+    }
 
-        try {
+    private static void testMain() {
+        System.out.println("[1] LZ78");
+        System.out.println("[2] LZSS");
+        System.out.println("[3] LZW");
+        System.out.println("[4] JPEG");
+        short opt = scanner.nextShort();
 
-            image = new Image();
-            image.readFromPpmFile(filename);
-
-            image.debug();
-            image.writeToPpmFile(filename_out);
-
-            image.toYCbCr();
-            image.debug();
-            image.writeToPpmFile(filename_YCbCr);
-
-            image.toRGB();
-            image.debug();
-            image.writeToPpmFile(filename_RGB);
-        } catch (IOException e) {
-            System.err.println("IO Exception");
-            e.printStackTrace();
-        } catch (Image.InvalidFileFormat invalidFileFormat) {
-            invalidFileFormat.printStackTrace();
+        switch (opt) {
+            case 1:
+                testLZ78();
+                break;
+            case 2:
+                testLZSS();
+                break;
+            case 3:
+                testLZW();
+                break;
+            case 4:
+                testJPEG();
+                break;
+            default:
+                quit();
         }
     }
 
+    private static void normalMain() {
+        System.out.println("Seleccioneu l'opció desitjada:");
+        System.out.println("[1] Comprimir");
+        System.out.println("[2] Descomprimir");
+        short opt = scanner.nextShort();
+
+        if(opt == 1) comprimir();
+        else if (opt == 2) descomprimir();
+        else quit();
+    }
+
+    private static void quit() {
+        System.out.println("Aborted");
+        System.exit(-1);
+    }
+
+    private static void comprimir() {
+        System.out.println("Tipus de compressió:");
+        System.out.println("[1] Automàtic");
+        System.out.println("[2] LZ78");
+        System.out.println("[3] LZSS");
+        System.out.println("[4] LZW");
+        System.out.println("[5] JPEG");
+        short opt = scanner.nextShort();
+
+        System.out.println("Introdueix el nom del fitxer:");
+        scanner.nextLine();
+        String filename = scanner.nextLine();
+
+        //TODO
+        switch (opt) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                quit();
+        }
+    }
+
+    private static void descomprimir() {
+        System.out.println("Introdueix el nom del fitxer:");
+        String filename = scanner.nextLine();
+
+        // TODO
+    }
+
+    private static void testLZ78() {}
+    private static void testLZSS() {}
+    private static void testLZW() {}
+    private static void testJPEG() {}
 }

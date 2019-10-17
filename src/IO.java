35baa1d8bdb5ae606c56c56217d4940f6cf1eac6
Albert Mercade <albertmercaplas@gmail.com>
2019-10-17 11:19:06 +0200

    Classes IO static

--- src/IO.java
+++ src/IO.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class IO {
-    public class reader {
+    public static class reader {
         private FileInputStream fin;
         private BufferedInputStream bin;
 
@@ -22,7 +22,7 @@ public class IO {
         }
     }
 
-    public class writer {
+    public static class writer {
         private FileOutputStream fout;
         private BufferedOutputStream bout;
 

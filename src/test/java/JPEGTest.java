2fa6abb861ddbd7db1b0dafbf498a30727050652
Leixb <abone9999@gmail.com>
2019-10-29 20:52:51 +0100

    Improved tests

--- src/test/java/JPEGTest.java
+++ src/test/java/JPEGTest.java
@@ -6,6 +6,8 @@ class JPEGTest {
 
     @Test
     void Dct() {
+        final int TOLERANCE = 2;
+
         byte[][] block = new byte[8][8];
         for (int i=0; i < 8; ++i) {
             for (int j=0; j < 8; ++j) {
@@ -15,13 +17,15 @@ class JPEGTest {
         double[][] dctBlock = JPEG.DCT.encode(block);
         byte[][] gotBlock = JPEG.DCT.decode(dctBlock);
 
-        for (int i=0; i < 8; ++i) {
-            for (int j=0; j < 8; ++j) {
-                int diff = Math.abs(gotBlock[i][j]-block[i][j]);
-                assertTrue(diff < 2);
-            }
-        }
+
+        assertTrue(
+                CompareWithTolerance(gotBlock, block, TOLERANCE),
+                String.format("DCT, diff >=%d", TOLERANCE)
+        );
     }
+
+    @Test
+    void Quantization() { }
 
     @Test
     void ZigZag() {
@@ -40,13 +44,22 @@ class JPEGTest {
 
         assertArrayEquals(line, gotLine);
         for (int i = 0; i < 8; ++i) {
-            assertArrayEquals(block[i], gotBlock[i]);
+            assertArrayEquals(block[i], gotBlock[i], "ZigZag");
         }
     }
 
     @Test
-    void quantizationValue() {
+    void RLE() {
+        byte[] smallInput = {11, 0, 24, 12, 30, 0, 0, 0, 0, 5, 0, 1, 2, 0, 0, 0, 0, 0, 0};
+        final byte[] expected = {0, 11, 1, 24, 0, 12, 0, 30, 4, 5, 1, 1, 0, 2, 0, 0};
+
+        byte[] input = new byte[64];
+        System.arraycopy(smallInput, 0, input, 0, smallInput.length);
+
+        assertArrayEquals(JPEG.RLE.encode(input), expected, "RLE encode");
+        assertArrayEquals(JPEG.RLE.decode(expected), input, "RLE decode");
     }
+
 
     private static final byte[][] sampleBlock = {
             {16,11,21,27,25,27,27,27},
@@ -60,47 +73,69 @@ class JPEGTest {
     };
 
     @Test
-    void RLE() {
-    }
-
-    @Test
     void all() {
-        byte[][] input = sampleBlock;
 
+        final int TOLERANCE = 15; // diff max to fail test
+
+        // Quantization parameters
         final short quality = 75;
         final boolean isChrominance = true;
+
+        byte[][] input = sampleBlock;
 
         double[][] DctEnc = JPEG.DCT.encode(input);
         byte[][] quantEnc = JPEG.Quantization.encode(quality, isChrominance, DctEnc);
         byte[] zigEnc = JPEG.ZigZag.encode(quantEnc);
         byte[] rleEnc = JPEG.RLE.encode(zigEnc);
+        byte[] huffEnc = JPEG.Huffman.encode(rleEnc);
 
-        byte[] rleDec = JPEG.RLE.decode(rleEnc);
+        byte[] huffDec = JPEG.Huffman.decode(huffEnc);
+        byte[] rleDec = JPEG.RLE.decode(huffDec);
         byte[][] zigDec = JPEG.ZigZag.decode(rleDec);
         double[][] quantDec = JPEG.Quantization.decode(quality, isChrominance, zigDec);
         byte[][] result = JPEG.DCT.decode(quantDec);
 
-        print("INPUT", input);
-        print("DCT enc", DctEnc);
-        print("Quant enc", quantEnc);
-        print("Zig enc", zigEnc);
-        print("RLE enc", rleEnc);
+        if (!CompareWithTolerance(input, result, TOLERANCE)) {
+            print("INPUT", input);
+            print("DCT enc", DctEnc);
+            print("Quant enc", quantEnc);
+            print("Zig enc", zigEnc);
+            print("RLE enc", rleEnc);
+            print("Huff enc", huffEnc);
 
-        print("RLE dec", rleDec);
-        print("Zig dec", zigDec);
-        print("Quant dec", quantDec);
-        print("DCT dec", result);
+            print("Huff dec", huffDec);
+            print("RLE dec", rleDec);
+            print("Zig dec", zigDec);
+            print("Quant dec", quantDec);
+            print("DCT dec", result);
 
-        boolean ok = true;
+            fail(String.format("ALL diff >%d, (quality=%d, isChrom=%b)", TOLERANCE, quality, isChrominance));
+        }
+    }
 
-        for (int i=0; i < 8; ++i) {
-            for (int j=0; j < 8; ++j) {
-                int diff = Math.abs(input[i][j]-result[i][j]);
-                ok = ok && diff < 3;
-            }
+    @Test
+    void encodeDecode() {
+        final int TOLERANCE = 15;
+
+        byte[] encoded = JPEG.encode(sampleBlock);
+        byte[][] result = JPEG.decode(encoded);
+
+        assertTrue(
+                CompareWithTolerance(sampleBlock, result, TOLERANCE),
+                String.format("encode -> decode diff >%d", TOLERANCE)
+        );
+    }
+
+    boolean CompareWithTolerance(byte[][] a, byte[][] b, int tolerance) {
+        if (a.length != b.length) return false;
+
+        for (int i=0; i < a.length; ++i) {
+            if (a[i].length != b[i].length) return false;
+            for (int j=0; j < a[i].length; ++j)
+                if (Math.abs(a[i][j]-b[i][j])>= tolerance) return false;
         }
 
-        assertTrue(ok);
+        return true;
     }
 
     void print(String name, byte[][] data) {
@@ -116,7 +151,7 @@ class JPEGTest {
         System.out.println(name);
         for (int i=0; i < data.length; ++i) {
             for (int j=0; j < data[i].length; ++j)
-                System.out.printf("%.3f ", data[i][j]);
+                System.out.printf("%.1f ", data[i][j]);
             System.out.println();
         }
     }

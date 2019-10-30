c80ccf0cbf1911859e859fac5739f6e5d98af58b
Leixb <abone9999@gmail.com>
2019-10-30 20:00:42 +0100

    Create generated dir for tests

--- src/test/java/PpmImageTest.java
+++ src/test/java/PpmImageTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PpmImageTest {
@@ -42,6 +44,7 @@ class PpmImageTest {
             System.out.printf("%d -> %d (%.2f%%)\n", original_size, compressed_size, (100*compressed_size/(double)original_size));
 
 
+            new File("generated/").mkdirs();
             img.writeFile("generated/" + outputFile);
         } catch (Exception e) {
             e.printStackTrace();

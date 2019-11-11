b61838f27748a25662a6b256d9d5bd6ad0d42bc2
Leixb <abone9999@gmail.com>
2019-11-11 20:37:38 +0100

    IO read/write bit a bit

--- src/main/java/Domini/IO.java
+++ src/main/java/Domini/IO.java
@@ -19,6 +19,27 @@ public class IO {
             return bin.read();
         }
 
+        int bitBuffer;
+        int bitMask;
+        public boolean readBit() throws IOException {
+            if (bitMask == 0 || bitMask > 0xFF) {
+                bitBuffer = read();
+                if (bitBuffer == -1) throw new EOFException();
+
+                bitMask = 1;
+            }
+
+            boolean bit = (bitBuffer & bitMask) != 0;
+
+            bitMask = bitMask << 1;
+
+            return bit;
+        }
+
+        void flushBitBuffer() {
+            bitMask = 0x100;
+        }
+
         public String readLine() throws IOException {
             StringBuffer sbuf = new StringBuffer();
 
@@ -61,7 +82,26 @@ public class IO {
             bout.write(b);
         }
 
+        int bitBuffer;
+        int bitMask;
+        public void writeBit(boolean bit) throws IOException {
+            if (bitMask == 0) {
+                writeBitBuffer();
+                bitBuffer = 0x00;
+                bitMask = 0x80;
+            }
+
+            if (bit) bitBuffer &= bitMask;
+
+            bitMask = bitMask >> 1;
+        }
+
+        private void writeBitBuffer() throws IOException {
+            bout.write((byte)bitBuffer);
+        }
+
         public void close () throws IOException {
+            writeBitBuffer();
             bout.flush();
             bout.close();
             fout.flush();

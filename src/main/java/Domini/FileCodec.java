47e3db65cbfdca2e7f030b5b38716d14b47ddd73
Leixb <abone9999@gmail.com>
2019-11-12 13:34:25 +0100

    Interfice FileCodec
    close #64

---
+++ src/main/java/Domini/FileCodec.java
@@ -0,0 +1,10 @@
+package Domini;
+
+interface FileCodec {
+    static void encode(IO.reader input, IO.writer output) {
+        return;
+    }
+    static void decode(IO.reader input, IO.writer output) {
+        return;
+    }
+}

b8cc1763dbb095b666144ce5f0a41e040ce43db2
gonzagod <alexgonzagodoy@gmail.com>
2019-11-12 13:14:57 +0100

    Afegit LZ78

---
+++ src/main/java/Domini/LZ78.java
@@ -0,0 +1,49 @@
+package Domini;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class LZ78 {
+    HashMap<String, Integer> dict = new HashMap<String, Integer>();
+
+    public void LZ78_compressor(String filename) throws IOException {
+        try (IO.reader input = new IO.reader(filename);
+             IO.writer output = new IO.writer("Compressed_" + filename)) {
+
+            String chars = "";
+            int num = 0;
+
+            int chin = input.read();
+            while (chin != -1) {
+                String charac = chars + (char) chin;
+                if (dict.get(charac) == null) {
+                    dict.put(charac, num);
+                    ++num;
+                    output.write(charac);
+                    //Escribir la entrada del dict en el archivo comprimido
+                    //o guardarlo para despues codificarlo en binario
+                } else {
+                    chars = charac;
+                }
+            }
+        }
+    }
+/*
+    public void LZ78_decompressor(String filename) throws IOException{
+        //En caso que se haya codificado en binario descodificarlo antes de leerlo
+        try (IO.reader input = new IO.reader(filename);
+             IO.writer output = new IO.writer("Decompressed_" + filename)) {
+
+            String chars = "";
+            int chin = input.read();
+            while (chin != -1) {
+                String charac = chars + (char) chin;
+                if (charac == "0") {
+                    //Escribir el siguiente caracter por la salida ya que no referencia ninguna entrada del diccionario
+                } else {
+                    chars=dict[charac];
+                }
+            }
+        }
+    }
+    */
+}

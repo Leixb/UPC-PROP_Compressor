497af168c7cb475edb32ef556a3d07daa483f62a
Albert Mercade <albertmercaplas@gmail.com>
2019-11-15 14:06:07 +0100

    Main (#90)
    * main mig fet
    
    * LZSS acabat
    
    * main mig fet
    
    * LZSS acabat
    
    * Control Domini funciona per LZSS
    
    * main/CtrlDomini en progres
    
    * Main/CtrlDomini/LZSS
    
    * main corregit

--- src/main/java/domini/CtrlDomini.java
+++ src/main/java/domini/CtrlDomini.java
@@ -1,4 +1,83 @@
 package domini;
 
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
 public class CtrlDomini {
+    public enum Alg {AUTOd, LZ78d, LZSSd, LZWd, JPEGd};
+    public static Statistics compress(Alg alg, String fileIn, String fileOut, Short quality) throws Exception {
+        Statistics stats = new Statistics();
+        switch(alg) {
+            case AUTOd:
+                break;
+            case LZ78d:
+                try(IO.Char.reader input = new IO.Char.reader(fileIn); IO.Bit.writer output = new IO.Bit.writer(fileOut)) {
+                    LZ78.compressor(input,output);
+                }
+                break;
+            case LZSSd:
+                try(IO.Char.reader input = new IO.Char.reader(fileIn); IO.Bit.writer output = new IO.Bit.writer(fileOut)) {
+                    stats.start();
+                    LZSS.compress(input,output);
+                    stats.stop();
+                    stats.initialFileSize(fileIn);
+                    stats.finalFileSize(fileOut);
+                }
+                break;
+            case LZWd:
+                try(IO.Char.reader input = new IO.Char.reader(fileIn); IO.Char.writer output = new IO.Char.writer(fileOut)) {
+                    LZW.compress(input,output);
+                }
+                break;
+            case JPEGd:
+                JPEG.compress(fileIn,fileOut,quality);
+                break;
+            default:
+        }
+        System.out.print("Time (s): ");
+        System.out.println(stats.getTime());
+        System.out.print("Compression (%): ");
+        System.out.println(stats.getPercentageCompressed());
+        return stats;
+    }
+
+    public static void decompress(String fileIn, String fileOut) throws Exception {
+        Alg alg;
+        int b;
+        try(IO.Byte.reader reader = new IO.Byte.reader(fileIn)){
+            b = reader.read();
+        }
+
+        if(b == -1) throw new EOFException();
+
+        if(b==0x78) alg = Alg.LZ78d;
+        else if(b==LZSS.MAGIC_BYTE) alg = Alg.LZSSd;
+        else if(b==0x11) alg = Alg.LZWd;
+        else if(b==0x92) alg = Alg.JPEGd;
+        else throw new Exception("Fitxer invàlid.");
+
+        switch(alg) {
+            case LZ78d:
+                try(IO.Bit.reader input = new IO.Bit.reader(fileIn); IO.Char.writer output = new IO.Char.writer(fileOut)) {
+                    LZ78.decompressor(input,output);
+                }
+                break;
+            case LZSSd:
+                try(IO.Bit.reader input = new IO.Bit.reader(fileIn); IO.Char.writer output = new IO.Char.writer(fileOut)) {
+                    LZSS.decompress(input,output);
+                }
+                break;
+            case LZWd:
+                try(IO.Char.reader input = new IO.Char.reader(fileIn); IO.Char.writer output = new IO.Char.writer(fileOut)) {
+                    LZW.decompress(input,output);
+                }
+                break;
+            case JPEGd:
+                JPEG.decompress(fileIn,fileOut,(short)0);
+                break;
+            default:
+        }
+    }
 }
--- src/main/java/domini/LZSS.java
+++ src/main/java/domini/LZSS.java
@@ -1,14 +1,20 @@
 package domini;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class LZSS {
-    final static int MAX_SIZE_SW = 8192; // 14 bits for offset
-    final static int MAX_LENGTH_COINCIDENCE = 65; // 6 bits for length
+    final static int MAX_SIZE_SW = 8192;
+    final static int MAX_LENGTH_COINCIDENCE = 65;
+    public final static byte MAGIC_BYTE = 0x55;
 
     public static void compress(IO.Char.reader input, IO.Bit.writer output) throws IOException {
+        output.write(MAGIC_BYTE);
 
+        int nBitsLength = (int)log2(MAX_LENGTH_COINCIDENCE-1);
+        int nBitsOffset = (int)log2(MAX_SIZE_SW);
+        int minLength = (1+nBitsLength+nBitsOffset)/17 + 1;
 
         ArrayList<Character> slidingWindow = new ArrayList<Character>();
         ArrayList<Character> actualCharacters = new ArrayList<Character>();
@@ -23,12 +29,10 @@ public class LZSS {
 
             if(index == -1 || length == MAX_LENGTH_COINCIDENCE) {
                 if(prevIndex != -1) actualCharacters.remove(actualCharacters.size()-1);
-                if(length >= 2) {
+                if(length >= minLength) {
                     output.write(true);
-                    boolean[] bits = intToNBits(prevIndex-1,14);
-                    for(int j = 13; j >= 0; --j) output.write(bits[j]);
-                    bits = intToNBits(length-2,6);
-                    for(int j = 5; j >= 0; --j) output.write(bits[j]);
+                    output.write(new BitSetL(prevIndex-1,nBitsOffset));
+                    output.write(new BitSetL(length-minLength,nBitsLength));
                     for(int i = 0; i < length; ++i) {
                         slidingWindow.add(actualCharacters.get(0));
                         actualCharacters.remove(0);
@@ -38,16 +42,9 @@ public class LZSS {
                     int lengthAux = actualCharacters.size();
                     for(int i = 0; i < lengthAux; ++i) {
                         output.write(false);
-                        boolean[] bits = intToNBits(actualCharacters.get(0),16);
-                        for(int j = 15; j >= 0; --j) output.write(bits[j]);
+                        output.write(new BitSetL(actualCharacters.get(0),16));
                         slidingWindow.add(actualCharacters.get(0));
                         actualCharacters.remove(0);
-                    }
-                }
-
-                if(slidingWindow.size() > MAX_SIZE_SW) {
-                    for(int i = 0; i < slidingWindow.size() - MAX_SIZE_SW; ++i) {
-                        slidingWindow.remove(0);
                     }
                 }
 
@@ -56,8 +53,7 @@ public class LZSS {
                     index = kmp(actualCharacters,slidingWindow);
                     if (index == -1) {
                         output.write(false);
-                        boolean[] bits = intToNBits(actualCharacters.get(0),16);
-                        for(int i = 15; i >= 0; --i) output.write(bits[i]);
+                        output.write(new BitSetL(actualCharacters.get(0),16));
                         slidingWindow.add(actualCharacters.get(0));
                         actualCharacters.remove(0);
                     }
@@ -80,27 +76,71 @@ public class LZSS {
                 prevIndex = index;
             }
 
+            if(slidingWindow.size() > MAX_SIZE_SW) {
+                int auxSize = slidingWindow.size();
+                for(int i = 0; i < auxSize - MAX_SIZE_SW; ++i) {
+                    slidingWindow.remove(0);
+                }
+            }
+
             c = input.read();
         }
 
         if(actualCharacters.size()>0) {
-            if(length >= 2) {
+            if(length >= minLength) {
                 output.write(true);
-                boolean[] bits = intToNBits(prevIndex-1,14);
-                for(int j = 13; j >= 0; --j) output.write(bits[j]);
-                bits = intToNBits(length-2,6);
-                for(int j = 5; j >= 0; --j) output.write(bits[j]);
+                output.write(new BitSetL(prevIndex-1,nBitsOffset));
+                output.write(new BitSetL(length-minLength,nBitsLength));
             }
             else {
                 int lengthAux = actualCharacters.size();
-                for(int i = 0; i < lengthAux; ++i) {
-                    output.write(false);
-                    boolean[] bits = intToNBits(actualCharacters.get(0),16);
-                    for(int j = 15; j >= 0; --j) output.write(bits[j]);
-                }
+                output.write(false);
+                output.write(new BitSetL(actualCharacters.get(0),16));
             }
         }
     }
+
+    public static void decompress(IO.Bit.reader input, IO.Char.writer output) throws IOException {
+        input.readByte();
+
+        int nBitsLength = (int)log2(MAX_LENGTH_COINCIDENCE-1);
+        int nBitsOffset = (int)log2(MAX_SIZE_SW);
+        int minLength = (1+nBitsLength+nBitsOffset)/17 + 1;
+
+        ArrayList<Character> slidingWindow = new ArrayList<Character>();
+
+        boolean c = input.read();
+        try {
+            while (true) {
+                if (c == true) {
+                    int index = input.readBitSet(nBitsOffset).asInt()+1;
+                    int length = input.readBitSet(nBitsLength).asInt()+minLength;
+                    int indexBase = slidingWindow.size() - index;
+                    for (int i = 0; i < length; ++i) {
+                        char cAux = slidingWindow.get(indexBase + i).charValue();
+                        output.write(cAux);
+                        slidingWindow.add(cAux);
+                    }
+                } else {
+                    char cAux = (char) input.readBitSet(16).asInt();
+                    output.write(cAux);
+                    slidingWindow.add(cAux);
+                }
+
+                if(slidingWindow.size() > MAX_SIZE_SW) {
+                    int auxSize = slidingWindow.size();
+                    for(int i = 0; i < auxSize - MAX_SIZE_SW; ++i) {
+                        slidingWindow.remove(0);
+                    }
+                }
+
+                c = input.read();
+            }
+        } catch (EOFException e) {
+
+        }
+    }
+
     private static void computeLPSArray (ArrayList<Character> pattern, int[] lps) {
         int patLength = pattern.size();
         int length = 0;
@@ -146,52 +186,7 @@ public class LZSS {
         return -1;
     }
 
-    private static boolean[] intToNBits(int cInt, int n) {
-        boolean[] bits = new boolean[n];
-        for(int i = 0; i < n; ++i) {
-            int aux = cInt % 2;
-            if(aux == 1) bits[i] = true;
-            cInt /= 2;
-        }
-        return bits;
-    }
-
-    public static void decompress(IO.Bit.reader input, IO.Char.writer output) throws IOException {
-        ArrayList<Character> slidingWindow = new ArrayList<Character>();
-        ArrayList<Character> actualCharacters = new ArrayList<Character>();
-
-        boolean c = input.read();
-        while(true) {
-            if(c == true) {
-                boolean[] bits = new boolean[14];
-                for(int i = 0; i < 14; ++i) bits[i] = input.read();
-                int index = bitsToInt(bits) + 1;
-                bits = new boolean[6];
-                for(int i = 0; i < 6; ++i) bits[i] = input.read();
-                int length = bitsToInt(bits) + 2;
-                int indexBase = slidingWindow.size() - index;
-                for(int i = 0; i < length; ++i) {
-                    char cAux = slidingWindow.get(indexBase+i).charValue();
-                    output.write(cAux);
-                    slidingWindow.add(cAux);
-                }
-            }
-            else {
-                boolean[] bits = new boolean[16];
-                for(int i = 0; i < 16; ++i) bits[i] = input.read();
-                char cAux = (char) bitsToInt(bits);
-                output.write(cAux);
-                slidingWindow.add(cAux);
-            }
-            c = input.read();
-        }
-    }
-
-    private static int bitsToInt(boolean[] bits) {
-        int size = bits.length, result = 0;
-        for(int i = 0; i < size; ++i) {
-            if(bits[i]) result += Math.pow(2,size - 1 - i);
-        }
-        return  result;
+    private static double log2(double n) {
+        return Math.log(n)/Math.log(2);
     }
 }

9d2501659fe29203ec89a0cfbf58a56d2f45af8e
Leix b <abone9999@gmail.com>
2019-11-17 23:47:14 +0100

    compress i decompress amb string, no IO.r/w (#116)
    * compress i decompress amb string, no IO.r/w
    
    close #115 
    close #127

--- src/test/java/LZWTest.java
+++ src/test/java/LZWTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import domini.LZW;
-import domini.IO;
 
 import java.io.File;
 
@@ -17,12 +16,8 @@ class LZWTest {
         final String aux = "generated/compressedFile.txt";
 
         try {
-            try (IO.Char.reader input = new IO.Char.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(aux)) {
-                LZW.compress(input,output);
-            }
-            try (IO.Bit.reader input = new IO.Bit.reader(aux); IO.Char.writer output = new IO.Char.writer(outputFile)) {
-                LZW.decompress(input,output);
-            }
+            LZW.compress(inputFile, aux);
+            LZW.decompress(aux, outputFile);
         } catch (Exception e) {
             e.printStackTrace();
             fail();

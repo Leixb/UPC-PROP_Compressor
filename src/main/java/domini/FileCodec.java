6d9dce291100bcd9a548e23989b8466c5fd6a820
Leix b <abone9999@gmail.com>
2019-11-17 20:11:18 +0100

    Delete FileCodec.java

--- src/main/java/domini/FileCodec.java
+++
@@ -1,6 +0,0 @@
-package domini;
-
-interface FileCodec {
-    public static void compress(final String inputFilename, final String outputFilename) { }
-    public static void decompress(final String inputFilename, final String outputFilename) { }
-}

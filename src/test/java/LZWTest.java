b1d3ab76f1f31a67e8cc3b84418243389805feb1
Leixb <abone9999@gmail.com>
2019-11-17 17:18:15 +0100

    Solo hacer prints en caso de fallo

--- src/test/java/LZWTest.java
+++ src/test/java/LZWTest.java
@@ -18,14 +18,10 @@ class LZWTest {
 
         try {
             try (IO.Char.reader input = new IO.Char.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(aux)) {
-                System.out.println("Starting compression...");
                 LZW.compress(input,output);
-                System.out.println("File successfully compressed.");
             }
             try (IO.Bit.reader input = new IO.Bit.reader(aux); IO.Char.writer output = new IO.Char.writer(outputFile)) {
-                System.out.println("Starting decompression...");
                 LZW.decompress(input,output);
-                System.out.println("File successfully decompressed.");
             }
         } catch (Exception e) {
             e.printStackTrace();

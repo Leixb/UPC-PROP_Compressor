2edf152b6511786bb5ceb092b1d8dfb844f455f6
Leix b <abone9999@gmail.com>
2019-11-17 16:53:13 +0100

    Fix JPEG tests (#125)

--- src/test/java/JPEGTest.java
+++ src/test/java/JPEGTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 class JPEGTest {
 
     private static final byte[][] sampleBlock = {
@@ -22,45 +24,32 @@ class JPEGTest {
 
     @Test
     void RW() throws Exception {
+        new File("generated/").mkdirs();
         Huffman huffAC = new Huffman(true, true);
         Huffman huffDC = new Huffman(false, true);
         short[] code = JPEGBlock.encode(sampleBlock);
-        try (IO.Bit.writer file = new IO.Bit.writer("testFile")) {
+        try (IO.Bit.writer file = new IO.Bit.writer("generated/testFile")) {
             JPEG.writeBlock(code, huffAC, huffDC, file);
             JPEG.writeBlock(code, huffAC, huffDC, file);
         }
 
         short[] result, resultB;
 
-        try (IO.Bit.reader file = new IO.Bit.reader("testFile")) {
+        try (IO.Bit.reader file = new IO.Bit.reader("generated/testFile")) {
             result = JPEG.readBlock(huffAC, huffDC, file);
             resultB = JPEG.readBlock(huffAC, huffDC, file);
         }
-
-        print("Original", code);
-        print("Result", result);
-        print("ResultB", result);
 
         assertArrayEquals(code, result);
         assertArrayEquals(code, resultB);
 
     }
 
-    void print(String name, short[] array) {
-        System.out.println(name);
-        for (int i=0; i < array.length; ++i)
-            System.out.printf("%04x ", array[i]);
-        System.out.println();
-        for (int i=0; i < array.length; ++i)
-            System.out.printf("%04d ", array[i]);
-        System.out.println();
-    }
-
     //@Test
     void test() {
         try {
-            JPEG.compress("images/boat.ppm", "boat.out", (short) 50);
-            JPEG.decompress("boat.out", "boat_rec.ppm");
+            JPEG.compress("images/boat.ppm", "generated/boat.out", (short) 50);
+            JPEG.decompress("generated/boat.out", "generated/boat_rec.ppm");
         } catch (Exception e) {
             e.printStackTrace();
             fail();

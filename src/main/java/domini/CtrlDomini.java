fddf42802003dc08f0f78539db225a3597055569
Albert Mercade <albertmercaplas@gmail.com>
2019-11-18 23:05:52 +0100

    Docu detallada (#185)
    * merdes del doxygent segurament
    
    * markdown de la especificacio

--- src/main/java/domini/CtrlDomini.java
+++ src/main/java/domini/CtrlDomini.java
@@ -4,7 +4,7 @@ import java.io.EOFException;
 
 /**
  * @author Albert Mercadé Plasencia / Aleix Boné
- * @brief Controlador del domini
+ * @brief Controlador del dominio
  */
 public class CtrlDomini {
     /**

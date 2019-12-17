787c2a55fb317944b61f806fb7944c2f4ff9f775
Leix b <abone9999@gmail.com>
2019-12-17 20:53:28 +0100

    Test cleans generated files (#239)
    close #229

--- src/test/java/LZSSTest.java
+++ src/test/java/LZSSTest.java
@@ -10,20 +10,22 @@ import java.io.*;
 class LZSSTest {
 
     @Test
-    void CompressDecompressFile() {
-        new File("generated/").mkdirs();
+    void compressDecompressFile() {
         final String inputFile = "texts/DonQuijote.txt";
-        final String outputFile = "generated/outLZSS.txt";
-        final String aux = "generated/compressedFileLZSS.piz";
 
         try {
-            try (IO.Byte.reader input = new IO.Byte.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(aux)) {
+            File tmpAux = File.createTempFile("LZSSTest", ".lzss.piz");
+            tmpAux.deleteOnExit();
+            File tmpOut = File.createTempFile("LZSSTest", ".txt");
+            tmpOut.deleteOnExit();
+
+            try (IO.Byte.reader input = new IO.Byte.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(tmpAux.getPath())) {
                 LZSS.compress(input, output);
             }
-            try (IO.Bit.reader input = new IO.Bit.reader(aux); IO.Byte.writer output = new IO.Byte.writer(outputFile)) {
+            try (IO.Bit.reader input = new IO.Bit.reader(tmpAux.getPath()); IO.Byte.writer output = new IO.Byte.writer(tmpOut.getPath())) {
                 LZSS.decompress(input, output);
             }
-            CheckCompDecomp.assertFileEquals(inputFile, outputFile);
+            CheckCompDecomp.assertFileEquals(inputFile, tmpOut.getPath());
         } catch (Exception e) {
             e.printStackTrace();
             fail();

031aa095a9f5d84da9e9a870c2edba57e4484b44
Leix b <abone9999@gmail.com>
2019-12-17 13:47:13 +0100

    Fixed tabs (expanded to 4 spaces) (#237)

--- src/test/java/LZ78Test.java
+++ src/test/java/LZ78Test.java
@@ -8,24 +8,24 @@ import persistencia.IO;
 import java.io.*;
 
 class LZ78Test {
-	@Test
-	void CompressDecompressFile() {
-		new File("generated/").mkdirs();
-		final String inputFile = "texts/DonQuijote.txt";
-		final String outputFile = "generated/outLZ78.txt";
-		final String aux = "generated/compressedFileLZ78.piz";
+    @Test
+    void CompressDecompressFile() {
+        new File("generated/").mkdirs();
+        final String inputFile = "texts/DonQuijote.txt";
+        final String outputFile = "generated/outLZ78.txt";
+        final String aux = "generated/compressedFileLZ78.piz";
 
-		try {
-			try (IO.Byte.reader input = new IO.Byte.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(aux)) {
-				LZ78.compress(input, output);
-			}
-			try (IO.Bit.reader input = new IO.Bit.reader(aux); IO.Byte.writer output = new IO.Byte.writer(outputFile)) {
-				LZ78.decompress(input, output);
-			}
-			CheckCompDecomp.assertFileEquals(inputFile, outputFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-	}
+        try {
+            try (IO.Byte.reader input = new IO.Byte.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(aux)) {
+                LZ78.compress(input, output);
+            }
+            try (IO.Bit.reader input = new IO.Bit.reader(aux); IO.Byte.writer output = new IO.Byte.writer(outputFile)) {
+                LZ78.decompress(input, output);
+            }
+            CheckCompDecomp.assertFileEquals(inputFile, outputFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
 }

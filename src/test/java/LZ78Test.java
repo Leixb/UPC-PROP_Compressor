fd094ad5d09e0be263e5b1ac08359544b8569a01
Leix b <abone9999@gmail.com>
2019-12-17 13:40:46 +0100

    CtrlDomini crea readers / writers dels LZ (#232)
    * CtrlDomini crea readers / writers de IO
    
    * JPEG uses reader/writer properly
    
    close #206

--- src/test/java/LZ78Test.java
+++ src/test/java/LZ78Test.java
@@ -3,26 +3,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import domini.LZ78;
+import persistencia.IO;
 
 import java.io.*;
 
 class LZ78Test {
+	@Test
+	void CompressDecompressFile() {
+		new File("generated/").mkdirs();
+		final String inputFile = "texts/DonQuijote.txt";
+		final String outputFile = "generated/outLZ78.txt";
+		final String aux = "generated/compressedFileLZ78.piz";
 
-    @Test
-    void CompressDecompressFile() {
-        new File("generated/").mkdirs();
-        final String inputFile = "texts/DonQuijote.txt";
-        final String outputFile = "generated/outLZ78.txt";
-        final String aux = "generated/compressedFileLZ78.piz";
-
-        try {
-            LZ78.compress(inputFile, aux);
-            LZ78.decompress(aux, outputFile);
+		try {
+			try (IO.Byte.reader input = new IO.Byte.reader(inputFile); IO.Bit.writer output = new IO.Bit.writer(aux)) {
+				LZ78.compress(input, output);
+			}
+			try (IO.Bit.reader input = new IO.Bit.reader(aux); IO.Byte.writer output = new IO.Byte.writer(outputFile)) {
+				LZ78.decompress(input, output);
+			}
 			CheckCompDecomp.assertFileEquals(inputFile, outputFile);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 }

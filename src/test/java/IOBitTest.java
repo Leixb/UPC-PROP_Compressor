d5a4d1ca0729e69954d103ee320aec087f6f2a27
Leixb <abone9999@gmail.com>
2019-12-17 14:57:19 +0100

    Test cleans generated files
    close #229

--- src/test/java/IOBitTest.java
+++ src/test/java/IOBitTest.java
@@ -9,15 +9,17 @@ import java.io.File;
 class IOBitTest {
     @Test
     void writeRead() {
-        new File("generated/").mkdirs();
         try {
-            try(IO.Bit.writer file = new IO.Bit.writer("generated/test")) {
+            File tmpFile = File.createTempFile("IOBitTest", ".hex");
+            tmpFile.deleteOnExit();
+
+            try(IO.Bit.writer file = new IO.Bit.writer(tmpFile.getPath())) {
                 file.write((byte) 0xFF);
 
                 file.write(new BitSetL(0xeeeeaaaa, 32));
                 file.write((int)0xabcd1234);
             }
-            try(IO.Bit.reader file = new IO.Bit.reader("generated/test")) {
+            try(IO.Bit.reader file = new IO.Bit.reader(tmpFile.getPath())) {
                 int r = file.readByte();
                 int n = file.readInt();
                 int m = file.readInt();

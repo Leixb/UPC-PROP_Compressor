d642202b0b0cb6dca7bbc68c607a09ab2619da79
Leix b <abone9999@gmail.com>
2019-12-17 13:16:46 +0100

    Mogut codi compartit dels tests a classe propia (#233)
    close #231

--- src/test/java/LZSSTest.java
+++ src/test/java/LZSSTest.java
@@ -18,23 +18,10 @@ class LZSSTest {
         try {
             LZSS.compress(inputFile, aux);
             LZSS.decompress(aux, outputFile);
-            checkCompressDecompress(inputFile,outputFile);
+			CheckCompDecomp.assertFileEquals(inputFile, outputFile);
         } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
-    }
-
-    void checkCompressDecompress (String original, String decompressed) throws IOException{
-        BufferedReader origin = new BufferedReader(new FileReader(original));
-        BufferedReader decomp = new BufferedReader(new FileReader(decompressed));
-        String checkLine;
-
-        checkLine = origin.readLine();
-        while (checkLine != null) {
-            assertEquals(checkLine, decomp.readLine());
-            checkLine = origin.readLine();
-        }
-        assertNull(decomp.readLine(),"Compressio i decompressio fallida!");
     }
 }

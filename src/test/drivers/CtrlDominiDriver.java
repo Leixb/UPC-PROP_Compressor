694d8502fe4e49513ae56c203d0aac7d4e4b2ba4
gonzagod <alexgonzagodoy@gmail.com>
2019-12-19 21:50:29 +0100

    CtrlDominiDriver i package private decompress(IO)

--- src/test/drivers/CtrlDominiDriver.java
+++ src/test/drivers/CtrlDominiDriver.java
@@ -6,33 +6,32 @@ import java.util.Scanner;
 class CtrlDominiDriver {
     private static Scanner scanner;
 
+    private static CtrlDomini ctdom;
+
+    private static void testGetInstance (){
+        System.out.println("getInstance: " + ctdom.getInstance());
+        System.out.println("DONE");
+    }
+
     public static void testCompress () {
         System.out.println("Compression algorithm: ");
         String[] options = {"LZ78", "LZSS", "LZW", "JPEG"};
         int num = prompt(options);
-        while (num<1 || num>4){
+        while (num < 1 || num > 4) {
             System.out.println("Invalid option.");
             num = prompt(options);
         }
-        CtrlDomini.Alg alg = CtrlDomini.Alg.values()[num-1];
         System.out.println("Input file (file to compress): ");
         String inputFile = scanner.next();
         System.out.println("Output file: ");
         String outputFile = scanner.next();
         Short quality = 0;
-        if ((num-1) == 4) {
+        if ((num - 1) == 4) {
             System.out.println("Compression quality: ");
             quality = scanner.nextShort();
         }
-
-
         try {
-            Statistics stats = new Statistics();
-            stats = CtrlDomini.compress(alg,inputFile,outputFile,quality);
-            System.out.println("Compress stats:");
-            System.out.println("time: " + stats.getTime());
-            System.out.println("iniFileSize: " + stats.getIniFileSize());
-            System.out.println("finFileSize: " + stats.getFinFileSize());
+            ctdom.compress(num, inputFile, outputFile, quality);
             System.out.println("DONE");
         } catch (Exception e) {
             System.out.println("CtrlDomini compress failed!");
@@ -45,19 +44,48 @@ class CtrlDominiDriver {
         String inputFile = scanner.next();
         System.out.println("Output file: ");
         String outputFile = scanner.next();
-
         try {
-            Statistics stats = new Statistics();
-            stats = CtrlDomini.decompress(inputFile,outputFile);
-            System.out.println("Decompress stats:");
-            System.out.println("time: " + stats.getTime());
-            System.out.println("iniFileSize: " + stats.getIniFileSize());
-            System.out.println("finFileSize: " + stats.getFinFileSize());
+            ctdom.decompress(inputFile, outputFile);
             System.out.println("DONE");
-        } catch (Exception e) {
+        } catch (Esception e) {
             System.out.println("CtrlDomini decompress failed!");
             e.printStackTrace();
         }
+    }
+
+    public static void testGetTime(){
+        System.out.println("getTime: " + ctdom.getTime());
+        System.out.println("DONE");
+    }
+
+    public static void testGetDeflated(){
+        System.out.println("getDeflated: " + ctdom.getDeflated());
+        System.out.println("DONE");
+    }
+
+    public static void testGetSpeedCompress(){
+        System.out.println("getSpeedCompress: " + ctdom.getSpeedCompress());
+        System.out.println("DONE");
+    }
+
+    public static void testGetInflated(){
+        System.out.println("getInflated: " + ctdom.getInflated());
+        System.out.println("DONE");
+    }
+
+    public static void testGetSpeedDecompress(){
+        System.out.println("getSpeedDecompress: " + ctdom.getSpeedDecompress());
+        System.out.println("DONE");
+    }
+
+    public static void testGetFileIn(){
+        System.out.println("getFileIn: " + ctdom.getFileIn());
+        System.out.println("DONE");
+    }
+
+    public static void testGetFileOut(){
+        System.out.println("getFileOut: " + ctdom.getFileOut());
+        System.out.println("DONE");
     }
 
     public static int prompt(String[] options) {
@@ -70,14 +98,28 @@ class CtrlDominiDriver {
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
 
-        String[] options = {"compress", "decompress", "exit"};
+        String[] options = {
+                "compress", "decompress",
+                "getInstance", "getTime",
+                "getDeflated", "getInflated",
+                "getSpeedCompress", "getSpeedDecompress",
+                "getFileIn", "getFileOut",
+                "exit"};
 
         int action = prompt(options);
 
-        while (action != 3) {
+        while (action != 11) {
 
             if (action == 1) testCompress();
             else if (action == 2) testDecompress();
+            else if (action == 3) testGetInstance();
+            else if (action == 4) testGetTime();
+            else if (action == 5) testGetDeflated();
+            else if (action == 6) testGetInflated();
+            else if (action == 7) testGetSpeedCompress();
+            else if (action == 8) testGetSpeedDecompress();
+            else if (action == 9) testGetFileIn();
+            else if (action == 10) testGetFileOut();
             else System.out.println("Invalid option");
 
             action = prompt(options);

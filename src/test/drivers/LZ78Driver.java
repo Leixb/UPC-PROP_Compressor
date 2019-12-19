2068437063cdc5795feba69944f5f04722b769ff
LEXHERRE <alexherrero1103@gmail.com>
2019-12-19 20:13:22 +0100

    LZ78Driver

--- src/test/drivers/LZ78Driver.java
+++ src/test/drivers/LZ78Driver.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 class LZ78Driver {
     private static Scanner scanner;
 
-    public static void testCompress() {
+    private static void testCompress() {
         System.out.print("Input file to compress: ");
         String inputFilename = scanner.next();
         System.out.print("Output file compressed: ");
         String outputFilename = scanner.next();
 
+        IO.Byte.reader input = new IO.Byte.reader(inputFilename);
+        IO.Bit.writer output = new IO.Bit.writer(outputFilename);
+
         try {
-            LZ78.compress(inputFilename,outputFilename);
+            LZ78.compress(input, output);
             System.out.println("Successful compression!");
         } catch (Exception e) {
             System.out.println("LZ78 compress failed!");
@@ -20,11 +23,14 @@ class LZ78Driver {
         }
     }
 
-    public static void testDecompress() {
+    private static void testDecompress() {
         System.out.print("Input file to decompress: ");
         String inputFilename = scanner.next();
         System.out.print("Output file decompressed: ");
         String outputFilename = scanner.next();
+
+        IO.Bit.reader input = new IO.Bit.reader(inputFilename);
+        IO.Byte.writer output = new IO.Byte.writer(outputFilename);
 
         try {
             LZ78.decompress(inputFilename, outputFilename);
@@ -35,9 +41,13 @@ class LZ78Driver {
         }
     }
 
-    public static int prompt(String[] options) {
+    private static void testGetMagicByte() {
+        System.out.print("getMagicByte: " + LZW.getMagicByte());
+    }
+
+    private static int prompt(String[] options) {
         for (int i = 1; i <= options.length; ++i)
-            System.out.printf("- [%d] : %s\n", i, options[i-1]);
+            System.out.printf("- [%d] : %s\n", i, options[i - 1]);
         System.out.printf("Chose one option (%d-%d): ", 1, options.length);
         return scanner.nextInt();
     }
@@ -45,14 +55,15 @@ class LZ78Driver {
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
 
-        String[] options = {"Compress", "Decompress", "Exit"};
+        String[] options = {"compress", "decompress", "getMagicByte", "exit"};
 
         int action = prompt(options);
 
-        while (action != 3) {
+        while (action != 4) {
 
             if (action == 1) testCompress();
             else if (action == 2) testDecompress();
+            else if (action == 3) testGetMagicByte();
             else System.out.println("Invalid option");
 
             action = prompt(options);
